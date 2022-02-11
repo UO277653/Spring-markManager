12554fd7948355037bb30ed9cabb55d34a948214
Stelian Adrian Stanci <UO277653@uniovi.es>
2022-02-11 19:51:49 +0100

    SDI-2021-212-2.3-Beans, servicios e inyección de dependencias.

--- src/main/java/com/uniovi/sdi2122212spring/controllers/MarksController.java
+++ src/main/java/com/uniovi/sdi2122212spring/controllers/MarksController.java
@@ -1,6 +1,8 @@
 package com.uniovi.sdi2122212spring.controllers;
 
 import com.uniovi.sdi2122212spring.entities.*;
+import com.uniovi.sdi2122212spring.services.MarksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,19 +12,29 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class MarksController {
 
+    @Autowired //Inyectar el servicio
+    private MarksService marksService;
+
     @RequestMapping("/mark/list")
     public String getList() {
-        return "Getting List";
+
+        return marksService.getMarks().toString();
     }
 
     @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
     public String setMark(@ModelAttribute Mark mark) {
-        return "added: " + mark.getDescription()
-                + " with score : " + mark.getScore()
-                + " id: " + mark.getId();
+        marksService.addMark(mark);
+        return "Ok";
     }
+
     @RequestMapping("/mark/details/{id}")
     public String getDetail(@PathVariable Long id) {
-        return "Getting Details =>" + id;
+        return marksService.getMark(id).toString();
+    }
+
+    @RequestMapping("/mark/delete/{id}")
+    public String deleteMark(@PathVariable Long id){
+        marksService.deleteMark(id);
+        return "Ok";
     }
 }
--- src/main/java/com/uniovi/sdi2122212spring/entities/Mark.java
+++ src/main/java/com/uniovi/sdi2122212spring/entities/Mark.java
@@ -4,22 +4,44 @@ public class Mark {
     private Long id;
     private String description;
     private Double score;
+
+    public Mark() {
+    }
+
+    public Mark(Long id, String description, Double score) {
+        this.id = id;
+        this.description = description;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Mark{" + "id=" + id + ", description='" + description + '\'' + ", score=" + score + '}';
+    }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public Double getScore() {
         return score;
     }
+
     public void setScore(Double score) {
         this.score = score;
     }
+
+
 }
