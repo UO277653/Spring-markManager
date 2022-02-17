a6ce90de16e0e69d01e71feb9edaab74d515045a
Stelian Adrian Stanci <UO277653@uniovi.es>
2022-02-17 13:39:37 +0100

    SDI-2122-212-3.1-Thymeleaf, fragmentos.

--- src/main/java/com/uniovi/sdi2122212spring/controllers/MarksController.java
+++ src/main/java/com/uniovi/sdi2122212spring/controllers/MarksController.java
@@ -58,4 +58,10 @@ public class MarksController {
         marksService.addMark(mark);
         return "redirect:/mark/details/"+id;
     }
+
+    @RequestMapping("/mark/list/update")
+    public String updateList(Model model){
+        model.addAttribute("markList", marksService.getMarks() );
+        return "mark/list :: tableMarks";
+    }
 }
