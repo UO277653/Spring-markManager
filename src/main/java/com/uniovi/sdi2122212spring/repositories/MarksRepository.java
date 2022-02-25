36e61a87b9dee9f2677c08d8d938ccd7b1a44c17
Stelian Adrian Stanci <UO277653@uniovi.es>
2022-02-25 23:04:55 +0100

    SDI-2122-212-4.5-SpringBoot-Búsqueda

--- src/main/java/com/uniovi/sdi2122212spring/repositories/MarksRepository.java
+++ src/main/java/com/uniovi/sdi2122212spring/repositories/MarksRepository.java
@@ -16,4 +16,10 @@ public interface MarksRepository extends CrudRepository<Mark, Long> {
 
     @Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC")
     List<Mark> findAllByUser(User user);
+
+    @Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1))")
+    List<Mark> searchByDescriptionAndName(String seachtext);
+
+    @Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1)) AND r.user = ?2")
+    List<Mark> searchByDescriptionNameAndUser(String seachtext, User user);
 }
